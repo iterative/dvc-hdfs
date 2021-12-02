88977a2f3b60b71b808afd911364833a4957d328
Ruslan Kuprieiev <kupruser@gmail.com>
2021-12-02 12:16:28 +0200

    fill out tests

--- dvc_hdfs/tests/test_dvc.py
+++ dvc_hdfs/tests/test_dvc.py
@@ -13,6 +13,7 @@ from dvc.testing.test_workspace import (  # noqa, pylint: disable=unused-import
 def cloud_name():
     return "hdfs"
 
+
 @pytest.fixture
 def remote(make_remote, cloud_name):
     yield make_remote(name="upstream", typ=cloud_name)
@@ -25,30 +26,29 @@ def workspace(make_workspace, cloud_name):
 
 @pytest.fixture
 def stage_md5():
-    raise NotImplementedError
+    return "ec0943f83357f702033c98e70b853c8c"
 
 
 @pytest.fixture
 def is_object_storage():
-    raise NotImplementedError
+    return False
 
 
 @pytest.fixture
 def dir_md5():
-    raise NotImplementedError
+    return "e6dcd267966dc628d732874f94ef4280.dir"
 
 
 @pytest.fixture
 def hash_name():
-    raise NotImplementedError
+    return "checksum"
 
 
 @pytest.fixture
 def hash_value():
-    raise NotImplementedError
+    return "000002000000000000000000a86fe4d846edc1bf4c355cb6112f141e"
 
 
 @pytest.fixture
 def dir_hash_value(dir_md5):
     raise NotImplementedError
-
