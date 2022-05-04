a5b814ec2780c9978b3c7217aab18453e37e71f9
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2022-05-04 17:43:39 +0200

    test_dvc: Add TestImportHDFS and TestAddHDFS.
    Fix unintentional skips.

--- dvc_hdfs/tests/test_dvc.py
+++ dvc_hdfs/tests/test_dvc.py
@@ -24,31 +24,29 @@ def workspace(make_workspace, cloud_name):
     yield make_workspace(name="workspace", typ=cloud_name)
 
 
-@pytest.fixture
-def stage_md5():
-    return "ec0943f83357f702033c98e70b853c8c"
+class TestImportHDFS(TestImport):
+    @pytest.fixture
+    def stage_md5(self):
+        return "ec0943f83357f702033c98e70b853c8c"
+
+    @pytest.fixture
+    def dir_md5(self):
+        return "e6dcd267966dc628d732874f94ef4280.dir"
+
+    @pytest.fixture
+    def is_object_storage(self):
+        return False
 
 
-@pytest.fixture
-def is_object_storage():
-    return False
+class TestAddHDFS(TestAdd):
+    @pytest.fixture
+    def hash_name(self):
+        return "checksum"
 
+    @pytest.fixture
+    def hash_value(self):
+        return "000002000000000000000000a86fe4d846edc1bf4c355cb6112f141e"
 
-@pytest.fixture
-def dir_md5():
-    return "e6dcd267966dc628d732874f94ef4280.dir"
-
-
-@pytest.fixture
-def hash_name():
-    return "checksum"
-
-
-@pytest.fixture
-def hash_value():
-    return "000002000000000000000000a86fe4d846edc1bf4c355cb6112f141e"
-
-
-@pytest.fixture
-def dir_hash_value(dir_md5):
-    pytest.skip("external outputs are broken for hdfs dirs")
+    @pytest.fixture
+    def dir_hash_value(self):
+        pytest.skip("external outputs are broken for hdfs dirs")
